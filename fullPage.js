d464260c4f833a98bdc95c324e09d77be972a271
jinii9 <jinii915@naver.com>
2024-08-29 11:02:18 +0900

    업데이트

---
+++ fullPage.js
@@ -0,0 +1,17 @@
+// var myFullpage = new fullpage("#fullpage", {
+//   // Get your license at https://alvarotrigo.com/fullPage/pricing
+//   licenseKey: "xxxxxxxxxxxxxxxxxxxxxxxxx",
+// });
+
+// new fullpage("#fullpage", {
+//   autoScrolling: true,
+//   //   navigation: true,
+//   // 필요에 따라 추가 옵션을 설정할 수 있습니다.
+// });
+
+new fullpage("#fullpage", {
+  autoScrolling: true,
+  navigation: true,
+  scrollingSpeed: 700, // 스크롤 속도 (밀리초 단위)
+  // 추가 옵션을 설정할 수 있습니다.
+});
